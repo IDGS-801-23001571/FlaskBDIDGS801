a595340c58602e51fd4e39166cde40b7ed3f33ec
eerick16 <erickgc0125@gmail.com>
2026-02-16 18:48:27 -0600

    Insertar 16/02/2026

--- app.py
+++ app.py
@@ -15,14 +15,33 @@ csrf=CSRFProtect()
 def page_not_found(e):
 	return render_template('404.html'),404
 
-@app.route("/alumnos",methods=['GET', 'POST'])
-def Alumnos():
+@app.route("/Alumnos",methods=['GET', 'POST'])
+def Alumnos2():
+	return render_template("Alumnos.html")
+
+@app.route('/Alumnos', methods=['GET', 'POST'])
+def Alumnos3():
+	create_form=forms.UserForm2(request.form)
+	if request.method=='POST':
+		alum=Alumnos3(nombre=create_form.nombre.data,
+				apaterno=create_form.apaterno.data,
+				email=create_form.email.data)
+		db.session.add(alum)
+		db.session.commit()
+		return redirect(url_for('index'))
 	return render_template("Alumnos.html")
 
 @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("index.html")
+	create_form=forms.UserForm2(request.form)
+	#FORM select * from alumnos
+	alumno=Alumnos.query.all()
+	return render_template("index.html",form=create_form,alumno=alumno)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	csrf.init_app(app)
+	db.init_app(app)
+	with app.app_context():
+		db.create_all()
+	app.run()
--- config.py
+++ config.py
@@ -8,5 +8,5 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://erick:root@127.0.0.1/bdidgs801'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@127.0.0.1/bdidgs801'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
