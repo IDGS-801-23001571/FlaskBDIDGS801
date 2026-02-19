ef672d095e034220c3e7d4083967c47731c4dfbe
eerick16 <erickgc0125@gmail.com>
2026-02-18 19:01:46 -0600

    modificar 18/02/2026

--- app.py
+++ app.py
@@ -9,39 +9,72 @@ from models import db, Alumnos
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
-csrf=CSRFProtect()
+db.init_app(app)
+csrf = CSRFProtect()
 
 @app.errorhandler(404)
 def page_not_found(e):
-	return render_template('404.html'),404
-
-@app.route("/Alumnos",methods=['GET', 'POST'])
-def Alumnos2():
-	return render_template("Alumnos.html")
-
-@app.route('/Alumnos', methods=['GET', 'POST'])
-def Alumnos3():
-	create_form=forms.UserForm2(request.form)
-	if request.method=='POST':
-		alum=Alumnos3(nombre=create_form.nombre.data,
-				apaterno=create_form.apaterno.data,
-				email=create_form.email.data)
-		db.session.add(alum)
-		db.session.commit()
-		return redirect(url_for('index'))
-	return render_template("Alumnos.html")
+	return render_template("404.html"),404
 
 @app.route("/")
 @app.route("/index")
 def index():
-	create_form=forms.UserForm2(request.form)
-	#FORM select * from alumnos
+	create_form = forms.UserForm2(request.form)
 	alumno=Alumnos.query.all()
 	return render_template("index.html",form=create_form,alumno=alumno)
 
+@app.route("/alumnos",methods=['GET','POST'])
+def Alumnos2():
+	create_form = forms.UserForm2(request.form)
+	if request.method == 'POST':
+		alum = Alumnos(nombre=create_form.nombre.data,
+				 	   apaterno=create_form.apaterno.data,
+					   email=create_form.email.data)
+		db.session.add(alum)
+		db.session.commit()
+		return redirect(url_for('index'))
+	return render_template("Alumnos.html",form = create_form)
+
+@app.route("/detalles", methods=['GET','POST'])
+def detalles():
+	create_form = forms.UserForm2(request.form)
+	if request.method == 'GET':
+		id=request.args.get('id')
+		# select * from alumnos where id == id
+		alum1 = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+		id = request.args.get('id')
+		nombre = alum1.nombre
+		apaterno=alum1.apaterno
+		email=alum1.email
+	return render_template('detalles.html',id=id,nombre=nombre,apaterno=apaterno,email=email)
+
+@app.route("/modificar",methods=['GET','POST'])
+def modificar():
+	create_form = forms.UserForm2(request.form)
+	if request.method == 'GET':
+		id = request.args.get('id')
+		# select * from alumnos where id == id
+		alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+		create_form.id.data=request.args.get('id')
+		create_form.nombre.data=str.rstrip(alum1.nombre)
+		create_form.apaterno.data=alum1.apaterno
+		create_form.email.data=alum1.email
+	if request.method == 'POST':
+		id=create_form.id.data
+		alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+		alum1.id=id
+		alum1.nombre=str.rstrip(create_form.nombre.data)
+		alum1.apaterno=create_form.apaterno.data
+		alum1.email=create_form.email.data
+		db.session.add(alum1)
+		db.session.commit()
+		return redirect(url_for('index'))
+	return render_template('modificar.html',form=create_form)
+
+
+
 if __name__ == '__main__':
 	csrf.init_app(app)
-	db.init_app(app)
 	with app.app_context():
 		db.create_all()
 	app.run()
